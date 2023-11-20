c3bc03a560ad49ebd61b702d8b9c58854d7dedfd
CSUMBNathan <ntrujillo@csumb.edu>
2023-11-20 02:07:35 -0800

    Added Type Converter

--- app/src/main/java/com/example/ataskmanager/DB/AppDataBase.java
+++ app/src/main/java/com/example/ataskmanager/DB/AppDataBase.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
 
 import com.example.ataskmanager.Task;
 
-@Database(entities = {Task.class},version = 1)
+@Database(entities = {Task.class},version = 2)
+@TypeConverters({DateTypeConverter.class})
 public abstract class AppDataBase extends RoomDatabase {
     public static final String DATABASE_NAME = "Task.db";
     public static final String TASK_TABLE = "task_table";
@@ -16,7 +19,7 @@ public abstract class AppDataBase extends RoomDatabase {
     private static volatile AppDataBase instance;
     private static final Object LOCK = new Object();
 
-    public abstract Task TaskDAO();
+    public abstract TaskDAO TaskDAO();
 
     public static AppDataBase getInstance(Context context){
         if(instance == null){
