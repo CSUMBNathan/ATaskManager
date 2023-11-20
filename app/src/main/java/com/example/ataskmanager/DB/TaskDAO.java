fab68a91f02879920c30daca11216023a01c0686
CSUMBNathan <ntrujillo@csumb.edu>
2023-11-20 02:09:11 -0800

    Added Query

--- app/src/main/java/com/example/ataskmanager/DB/TaskDAO.java
+++ app/src/main/java/com/example/ataskmanager/DB/TaskDAO.java
@@ -3,9 +3,12 @@ package com.example.ataskmanager.DB;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.Update;
 
 import com.example.ataskmanager.Task;
+
+import java.util.List;
 
 @Dao
 public interface TaskDAO {
@@ -18,5 +21,13 @@ public interface TaskDAO {
 
     @Delete
     void delete(Task task);
+
+    @Query("SELECT * FROM " + AppDataBase.TASK_TABLE + " ORDER BY mDate desc")
+    List<Task> getTasks();
+
+    @Query("SELECT * FROM " + AppDataBase.TASK_TABLE + " WHERE mTaskId = :taskId")
+    List<Task> getLogById(int taskId);
+
+
 
 }
