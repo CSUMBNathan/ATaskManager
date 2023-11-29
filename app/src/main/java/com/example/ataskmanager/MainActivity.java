5b2315ab40423f2e216974e14e116969abd44116
CSUMBNathan <ntrujillo@csumb.edu>
2023-11-28 16:51:18 -0800

    implemented logout functionality.

--- app/src/main/java/com/example/ataskmanager/MainActivity.java
+++ app/src/main/java/com/example/ataskmanager/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.ataskmanager;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
@@ -15,16 +16,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
 import com.example.ataskmanager.DB.AppDataBase;
 import com.example.ataskmanager.DB.TaskDAO;
 import com.example.ataskmanager.databinding.ActivityMainBinding;
 
 import java.util.List;
-import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String USER_ID_KEY = "com.example.ataskmanager.userIdKey";
+    private static final String PREFENCES_KEY = "com.example.ataskmanager.PREFENCES_KEY";
     ActivityMainBinding binding;
 
     TextView mTaskDetails;
@@ -33,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     EditText mDescription;
 
     Button mSubmit;
+    Button mLogout;
 
     TaskDAO mTaskDAO;
 
@@ -40,29 +46,29 @@ public class MainActivity extends AppCompatActivity {
 
     private int mUserId = -1;
 
+    private SharedPreferences mPreferences = null;
+    private User mUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        getDataBase();
+        checkForUser();
+        loginUser(mUserId);
 
         mTaskDetails = binding.textViewTaskDetails;
         mSubmit = binding.buttonSubmit;
         mEvent = binding.editTextEvent;
         mDate = binding.editTextDate;
         mDescription = binding.editTextDescription;
-        //mReturn = binding.
+        mLogout = binding.buttonLogoutMain;
 
         mTaskDetails.setMovementMethod(new ScrollingMovementMethod());
 
-        mTaskDAO = Room.databaseBuilder(this, AppDataBase.class, AppDataBase.DATABASE_NAME)
-                .allowMainThreadQueries()
-                .build()
-                .TaskDAO();
 
         refreshDisplay();
 
@@ -75,21 +81,109 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
     } //end of onCreate
 
 
+    private void loginUser(int userId) {
+        mUser = mTaskDAO.getUserByUserId(userId);
+        addUserToPreference(userId);
+        //invalidateOptionsMenu();
+    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.logout_click) {
-            Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if(mUser != null){
+            MenuItem item = menu.findItem(R.id.logout_click);
+            item.setTitle(mUser.getUserName());
         }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    private void clearUserFromIntent() {
+        getIntent().putExtra(USER_ID_KEY,-1);
+    }
+
+    private void clearUserFromPref() {
+        addUserToPreference(-1);
+    }
+
+    private void addUserToPreference(int userId){
+        if(mPreferences == null){
+            getPrefs();
+        }
+        SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putInt(USER_ID_KEY, userId);
+        editor.apply();
+    }
+
+    private void getDataBase() {
+        mTaskDAO = Room.databaseBuilder(this, AppDataBase.class, AppDataBase.DATABASE_NAME)
+                .allowMainThreadQueries()
+                .build()
+                .TaskDAO();
+    }
+
+    private void checkForUser() {
+        mUserId = getIntent().getIntExtra(USER_ID_KEY,-1);
+
+        if(mUserId != -1){
+            return;
+        }
+
+        if(mPreferences == null){
+            getPrefs();
+        }
+
+        mUserId = mPreferences.getInt(USER_ID_KEY, -1);
+
+        if(mUserId != -1){
+            return;
+        }
+
+        List<User> users = mTaskDAO.getAllUsers();
+        if(users.size() <= 0){
+            User defaultUser = new User("daclink","daclink");
+            mTaskDAO.insert(defaultUser);
+        }
+
+        Intent intent = LoginActivity.intentFactory(this);
+        startActivity(intent);
+    }
+
+    private void getPrefs(){
+        mPreferences = this.getSharedPreferences(PREFENCES_KEY, Context.MODE_PRIVATE);
+    }
+
+    private void logoutUser() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setMessage("Are you sure you want to logout?");
+        alertBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        clearUserFromIntent();
+                        clearUserFromPref();
+                        mUserId = -1;
+                        checkForUser();
+                    }
+                });
+        alertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
     }
 
     private void submitTask(){
+        System.out.println("submit task running");
         String event = mEvent.getText().toString();
         String date = mDate.getText().toString();
         String details = mDescription.getText().toString();
@@ -97,21 +191,47 @@ public class MainActivity extends AppCompatActivity {
         Task task = new Task(event, date, details, mUserId);
 
         mTaskDAO.insert(task);
+        System.out.println(task.toString());
+        System.out.println("mUserId: "+ mUserId);
+        System.out.println("submit task end");
+
+        refreshDisplay();
+
+        mEvent.getText().clear();
+        mDate.getText().clear();
+        mDescription.getText().clear();
     }
 
 
     private void refreshDisplay(){
-        mTaskList = mTaskDAO.getTasks();
+        System.out.println("refresh display started");
+        mTaskList = mTaskDAO.getTaskByUserId(mUserId);
+//        mTaskList = mTaskDAO.getTaskById(mUserId);
         if(!mTaskList.isEmpty()){
             StringBuilder sb = new StringBuilder();
             for(Task task:mTaskList){
                 sb.append(task.toString());
             }
+            System.out.println("refresh to string: " + sb.toString());
             mTaskDetails.setText(sb.toString());
-            System.out.println(sb.toString());
-        }else{
+         }else{
+            System.out.println("Refresh else: asdfsadfsadf");
             mTaskDetails.setText("Make some tasks lazybones.");
         }
+    }
+
+    public static Intent intentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(USER_ID_KEY, userId);
+
+        return intent;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.task_menu,menu);
+        return true;
     }
 
 }
