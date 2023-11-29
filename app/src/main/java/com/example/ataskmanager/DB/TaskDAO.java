15a84828475105cad972b477ca2a168c588edcc8
CSUMBNathan <ntrujillo@csumb.edu>
2023-11-28 16:52:08 -0800

    Added user object to this.

--- app/src/main/java/com/example/ataskmanager/DB/TaskDAO.java
+++ app/src/main/java/com/example/ataskmanager/DB/TaskDAO.java
@@ -7,6 +7,7 @@ import androidx.room.Query;
 import androidx.room.Update;
 
 import com.example.ataskmanager.Task;
+import com.example.ataskmanager.User;
 
 import java.util.List;
 
@@ -23,11 +24,30 @@ public interface TaskDAO {
     void delete(Task task);
 
     @Query("SELECT * FROM " + AppDataBase.TASK_TABLE + " ORDER BY mDate desc")
-    List<Task> getTasks();
+    List<Task> getAllTasks();
 
     @Query("SELECT * FROM " + AppDataBase.TASK_TABLE + " WHERE mTaskId = :taskId")
-    List<Task> getLogById(int taskId);
+    List<Task> getTaskById(int taskId);
 
+    @Query("SELECT * FROM " + AppDataBase.TASK_TABLE + " WHERE mUserId = :userId ORDER BY mDate desc")
+    List<Task> getTaskByUserId(int userId);
 
+    @Insert
+    void insert(User...users);
+
+    @Update
+    void update(User...users);
+
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT * FROM " + AppDataBase.USER_TABLE)
+    List<User> getAllUsers();
+
+    @Query("SELECT * FROM " + AppDataBase.USER_TABLE + " WHERE mUserName = :username")
+    User getUserByUsername(String username);
+
+    @Query("SELECT * FROM " + AppDataBase.USER_TABLE + " WHERE mUserId = :userId")
+    User getUserByUserId(int userId);
 
 }
