91828b1a0c509781ef84bfd08b2348cddbb58d85
CSUMBNathan <ntrujillo@csumb.edu>
2023-11-30 17:37:25 -0800

    Added method to sign up

--- app/src/main/java/com/example/ataskmanager/LoginActivity.java
+++ app/src/main/java/com/example/ataskmanager/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.ataskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mPasswordField;
 
     private Button mButton;
+    private Button mSignUpButton;
 
     private TaskDAO mTaskDAO;
 
@@ -38,14 +41,11 @@ public class LoginActivity extends AppCompatActivity {
 
         getDatabase();
 
-
     }
 
     private void wireUpDisplay() {
-        mUsernameField = findViewById(R.id.editTextUsernameLoginActivity);
-        mPasswordField = findViewById(R.id.editTextLoginPassword);
 
-        mButton = findViewById(R.id.buttonLogin);
+        findFieldsById();
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,10 +58,65 @@ public class LoginActivity extends AppCompatActivity {
                         Intent intent = MainActivity.intentFactory(getApplicationContext(),mUser.getUserId());
                         startActivity(intent);
                     }
-            }
+                }
             }
         });
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpDialog();
+            }
+        });
+
+    }   //end of wired display
+
+
+    private void showSignUpDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Sign Up");
+
+        // Inflate the layout for the dialog
+        View view = getLayoutInflater().inflate(R.layout.activity_signup, null);
+        builder.setView(view);
+
+        final EditText usernameEditText = view.findViewById(R.id.editTextUsernameSignUp);
+        final EditText passwordEditText = view.findViewById(R.id.editTextPasswordSignUp);
+        final EditText confirmPasswordEditText = view.findViewById(R.id.editTextConfirmPasswordSignUp);
+
+        builder.setPositiveButton("Sign Up", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirmPassword = confirmPasswordEditText.getText().toString();
+
+                if (password.equals(confirmPassword)) {
+                    signUp(username, password);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
     }
+
+    private void signUp(String username, String password) {
+        // Perform sign-up logic here, for example, add the new user to the database
+        User newUser = new User(username, password);
+        mTaskDAO.insert(newUser);
+
+        Toast.makeText(LoginActivity.this, "Sign Up successful", Toast.LENGTH_SHORT).show();
+    }
+
     private boolean validatePassword(){
         return mUser.getPassword().equals(mPassword);
     }
@@ -92,4 +147,13 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(context, LoginActivity.class);
         return intent;
     }
+
+    private void findFieldsById(){
+        mUsernameField = findViewById(R.id.editTextUsernameLoginActivity);
+        mPasswordField = findViewById(R.id.editTextLoginPassword);
+        mButton = findViewById(R.id.buttonLogin);
+        mSignUpButton = findViewById(R.id.buttonSignUp);
+
+    }
+
 }
